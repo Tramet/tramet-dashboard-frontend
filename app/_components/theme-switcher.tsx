df943282c0f61838d00668dd563b836dd70c5a52
Diego Perez <darksilent518@gmail.com>
2025-03-31 12:26:35 -0600

    style: update theme switcher and global styles with corporate colors

--- app/_components/theme-switcher.tsx
+++ app/_components/theme-switcher.tsx
@@ -10,14 +10,15 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@trm/_components/ui/dropdown-menu";
-import { CheckIcon, Palette } from "lucide-react";
+import { CheckIcon, Palette, Sun, Moon } from "lucide-react";
 
 interface MenuItemProps {
   themeName: string;
   children: React.ReactNode;
+  icon?: React.ReactNode;
 }
 
-const MenuItem = ({ themeName, children }: MenuItemProps) => {
+const MenuItem = ({ themeName, children, icon }: MenuItemProps) => {
   const { setTheme, theme } = useTheme();
 
   const handleClick = () => {
@@ -25,8 +26,9 @@ const MenuItem = ({ themeName, children }: MenuItemProps) => {
   };
 
   return (
-    <DropdownMenuItem onClick={handleClick}>
-      <span>{children}</span>
+    <DropdownMenuItem onClick={handleClick} className="flex items-center gap-2">
+      {icon && <span className="flex items-center justify-center w-4 h-4">{icon}</span>}
+      <span className="flex-grow">{children}</span>
       {theme === themeName && <CheckIcon className="ml-2 h-4 w-4" />}
     </DropdownMenuItem>
   );
@@ -39,14 +41,14 @@ export const ThemeSwitcher: React.FC = () => {
         <Button
           variant="outline"
           size="icon"
-          className="rounded-full p-0 bg-foreground hover:bg-muted text-background hover:text-muted-foreground border-0 outline-none">
+          className="rounded-full p-0 bg-accent hover:bg-muted text-accent-foreground hover:text-muted-foreground border-0 outline-none">
           <Palette className="w-6 h-6" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="z-[99998]">
-        <MenuItem themeName="light">Light</MenuItem>
-        <MenuItem themeName="dark">Dark</MenuItem>
-        <MenuItem themeName="custom">Tramet Light</MenuItem>
+      <DropdownMenuContent align="end" className="z-[99998] ">
+        <MenuItem themeName="light" icon={<Sun className="h-4 w-4" />}>Claro</MenuItem>
+        <MenuItem themeName="dark" icon={<Moon className="h-4 w-4" />}>Oscuro</MenuItem>
+        <MenuItem themeName="custom" icon={<Palette className="h-4 w-4" />}>Tramet Claro</MenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
