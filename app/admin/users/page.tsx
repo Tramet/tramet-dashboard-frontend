c962477900b319d8ab5fd09db1127cc57e87865b
Diego Perez <darksilent518@gmail.com>
2025-03-31 12:27:11 -0600

    feat: update admin users page and dependencies with corporate styling

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,10 +1,12 @@
 import { AdminUsersTable } from "@trm/_components/admin-users-table";
+import { Button } from "@trm/_components/ui/button";
 import React from "react";
 
 const AdminUsers = () => {
   return (
     <div>
       <h2 className="text-3xl font-bold tracking-tight my-4">Usuarios</h2>
+
       <AdminUsersTable />
     </div>
   );
